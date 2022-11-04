24e3592312f64237f5b96c04eb879adf73699975
kitsuyy <lozovoi29111995@gmail.com>
2022-11-04 16:57:32 +0400

    Fix random element

--- R-spm/Services/DataAnimations.swift
+++ R-spm/Services/DataAnimations.swift
@@ -4,69 +4,15 @@
 //
 //  Created by Kirill Lozovoi on 11/1/22.
 //
+import SpringAnimation
 
 struct DataAnimation {
 
     static let dataAnimation = DataAnimation()
     
-    let animations = [
-        "pop",
-        "slideLeft",
-        "slideRight",
-        "slideDown",
-        "slideUp",
-        "squeezeLeft",
-        "squeezeRight",
-        "squeezeDown",
-        "squeezeUp",
-        "fadeIn",
-        "fadeOut",
-        "fadeOutIn",
-        "fadeInLeft",
-        "fadeInRight",
-        "fadeInDown",
-        "fadeInUp",
-        "zoomIn",
-        "zoomOut",
-        "fall",
-        "shake",
-        "flipX",
-        "flipY",
-        "morph",
-        "squeeze",
-        "flash",
-        "wobble",
-        "swing"
-    ]
+    let animations = AnimationPreset.allCases
     
-    let curvers = [
-        "easeOut",
-        "easeInOut",
-        "linear",
-        "spring",
-        "easeInSine",
-        "easeOutSine",
-        "easeInOutSine",
-        "easeInQuad",
-        "easeOutQuad",
-        "easeInOutQuad",
-        "easeInCubic",
-        "easeOutCubic",
-        "easeInOutCubic",
-        "easeInQuart",
-        "easeOutQuart",
-        "easeInOutQuart",
-        "easeInQuint",
-        "easeOutQuint",
-        "easeInOutQuint",
-        "easeInExpo",
-        "easeOutExpo",
-        "easeInOutExpo",
-        "easeInCirc",
-        "easeOutCirc",
-        "easeInOutCirc",
-        "easeInBack",
-        "easeOutBack",
-        "easeInOutBack"
-        ]
+    let curvers = AnimationCurve.allCases
+    
+    private init() {}
 }
